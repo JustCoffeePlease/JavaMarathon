8cadbdeb47d814f721b390714671a1c930fdcf2e
Алексей <Vaas19.96@yandex.ru>
2022-12-03 08:45:11 +0300

    Complete day0 and day1 tasks.

--- src/main/java/day1/Task4.java
+++ src/main/java/day1/Task4.java
@@ -3,5 +3,13 @@ package day1;
 public class Task4 {
     public static void main(String[] args) {
 
+        int year = 1980;
+
+        System.out.println("Оллимпиада " + year + " года");
+        while (year < 2020) {
+
+            year +=4;
+            System.out.println("Оллимпиада " + year + " года");
+        }
     }
 }
--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
-
+        int year = 1980;
+        System.out.println("Оллимпиада " + year + " года");
+        for (int i = 0; i < 10; i++){
+            year +=4;
+            System.out.println("Оллимпиада " + year + " года");
+        }
     }
 }
