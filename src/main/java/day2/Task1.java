165898c1cc1cecd8608f6d841923c47c915525e7
Алексей <Vaas19.96@yandex.ru>
2022-12-04 16:55:48 +0300

    Day 2 is done and checked

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -5,5 +5,25 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите целое число: ");
+        int number = scanner.nextInt();
+
+        // Малоэтажный дом
+        if (number < 5 && number > 0) {
+            System.out.println("Малоэтажный дом");
+        } else if
+        // Среднеэтажный дом
+            (number < 9 && number > 4) {
+                System.out.println("Среднеэтажный дом");
+        } else if
+        // Многоэтажный дом
+            (number > 8) {
+                System.out.println("Многоэтажный дом");
+        }
+         else {
+            System.out.println("Ошибка ввода");
+        }
+
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -5,5 +5,22 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a < b) {
+
+            while (a < b) {
+                a++;
+                if (a % 5 == 0 && a % 10 != 0) {
+                    System.out.print(a + " ");
+                }
+            }
+        }
+        else {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
