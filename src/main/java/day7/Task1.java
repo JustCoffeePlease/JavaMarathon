7afac1d2a0bab21e604554b05f4439772e563a1d
Алексей <Vaas19.96@yandex.ru>
2022-12-10 16:28:47 +0300

    Day 7 is done

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -3,5 +3,9 @@ package day7;
 public class Task1 {
     public static void main(String[] args) {
 
+        Airplane plainBoieng = new Airplane("Boieng", 1985, 88, 65);
+        Airplane plainIl = new Airplane("Ilushin", 1996, 110, 86);
+
+        Airplane.compareAirplanes(plainBoieng, plainIl);
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -3,5 +3,22 @@ package day7;
 public class Task2 {
     public static void main(String[] args) {
 
+        Player player1 = new Player(80);
+        Player player2 = new Player(90);
+        Player player3 = new Player(95);
+        Player player4 = new Player(98);
+        Player player5 = new Player(91);
+        Player player6 = new Player(84);
+
+        Player.info();
+        System.out.println("-------------------------------");
+
+        while (player1.getStamina() > 0){
+            player1.run();
+        }
+
+        System.out.println(player1.getStamina());
+
+        Player.info();
     }
 }
