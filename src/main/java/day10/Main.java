7dcf02ed344e76bc5b75ccaf446e68e2340f3f51
Алексей <Vaas19.96@yandex.ru>
2022-12-18 01:57:22 +0300

    Day 10 is done with bad array sorting

--- src/main/java/day10/Main.java
+++ src/main/java/day10/Main.java
@@ -14,21 +14,44 @@ public class Main {
         Node root = new Node(20);
 
         binaryTree.setRoot(root);
+        binaryTree.addValueInArray(0, 20);
 
         binaryTree.addNode(14, root);
-        binaryTree.addNode(11, root);
-        binaryTree.addNode(5, root);
-        binaryTree.addNode(8, root);
-        binaryTree.addNode(16, root);
-        binaryTree.addNode(15, root);
-        binaryTree.addNode(18, root);
-        binaryTree.addNode(23, root);
-        binaryTree.addNode(22, root);
-        binaryTree.addNode(27, root);
-        binaryTree.addNode(24, root);
-        binaryTree.addNode(150, root);
+        binaryTree.addValueInArray(1, 14);
 
-        System.out.println("--------------------------------------------------");
+        binaryTree.addNode(11, root);
+        binaryTree.addValueInArray(2, 11);
+
+        binaryTree.addNode(5, root);
+        binaryTree.addValueInArray(3, 11);
+
+        binaryTree.addNode(8, root);
+        binaryTree.addValueInArray(4,8);
+
+        binaryTree.addNode(16, root);
+        binaryTree.addValueInArray(5,16);
+
+        binaryTree.addNode(15, root);
+        binaryTree.addValueInArray(6,15);
+
+        binaryTree.addNode(18, root);
+        binaryTree.addValueInArray(7,18);
+
+        binaryTree.addNode(23, root);
+        binaryTree.addValueInArray(8,23);
+
+        binaryTree.addNode(22, root);
+        binaryTree.addValueInArray(9,22);
+
+        binaryTree.addNode(27, root);
+        binaryTree.addValueInArray(10,27);
+
+        binaryTree.addNode(24, root);
+        binaryTree.addValueInArray(11,24);
+
+        binaryTree.addNode(150, root);
+        binaryTree.addValueInArray(12,150);
+
         binaryTree.sortNodes();
 
 
--- src/main/java/day10/Node.java
+++ src/main/java/day10/Node.java
@@ -1,15 +1,22 @@
 package day10;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Node {
 
-    Node root;
+
+    Node root; // Значение корневого узла
     int value; // Значение, которое находится внутри узла
     Node left; // Левый потомок
     Node right; // Правый потомок
+
+    int i = 0;
+    int arraySize = 13; // Размер массива
+    int[] dataArray = new int[arraySize];
 
     // Пустой конструктор узла
     public Node() {
@@ -39,33 +46,37 @@ public class Node {
 
         if (this.root.equals(root)) {
             Node stepNode = this.root;
+
+
             while (true) {
                 // Случай 1: У исследуемого узла значение больше чем вводимое
                 //           и нет потомков слева
                 if (stepNode.value - value > 0 && stepNode.left == null) {
                     stepNode.left = new Node(value);
-                    System.out.println(stepNode.left.value);
+//                    addValueInArray(stepNode.left.value);
+//                    System.out.println(stepNode.left.value);
                     break;
                 }
                 // Случай 2: У исследуемого узла значение или равно чем вводимое
                 //           и нет потомков справа
                 else if (stepNode.value - value <= 0 && stepNode.right == null) {
                     stepNode.right = new Node(value);
-                    System.out.println(stepNode.right.value);
+//                    addValueInArray(stepNode.right.value);
+//                    System.out.println(stepNode.right.value);
                     break;
                 }
                 // Случай 3: У исследуемого узла значение больше чем вводимое
                 //           и есть потомки слева
                 else if (stepNode.value - value > 0 && stepNode.left != null) {
                     stepNode = stepNode.left;
-                    System.out.println(stepNode.value);
+//                    System.out.println(stepNode.value);
 //                    break;
                 }
                 // Случай 3: У исследуемого узла значение меньше или равно чем вводимое
                 //           и есть потомки справа
                 else if (stepNode.value - value <= 0 && stepNode.right != null) {
                     stepNode = stepNode.right;
-                    System.out.println(stepNode.value);
+//                    System.out.println(stepNode.value);
 //                    break;
                 }
             }
@@ -78,9 +89,13 @@ public class Node {
     // Метод, сортирующий данные древа и возвращающий StringBuilder данных
     public void sortNodes() {
 
-        StringBuilder dataList = new StringBuilder(Integer.toString(root.value));
+        System.out.print("Неотсортированный массив данных: ");
+        System.out.print(Arrays.toString(dataArray));
 
-        System.out.println(dataList.toString());
+        System.out.println();
+        Arrays.sort(dataArray);
+        System.out.print("Отсортированный массив данных: ");
+        System.out.print(Arrays.toString(dataArray));
     }
 
     // Вспомогательный метод, добавляющий в массив дополнительный элемент
@@ -90,5 +105,10 @@ public class Node {
         list.add(element);
 
         return list.toArray(new Integer[0]);
+    }
+
+    // Вспомогательный метод, добавляющий значения новых узлов в массива
+    public void addValueInArray(int i, int value) {
+        dataArray[i] = value;
     }
 }
