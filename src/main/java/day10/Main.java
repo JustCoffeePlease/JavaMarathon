659a34c9630d42c1199f6bf577c63365af367d65
Алексей <Vaas19.96@yandex.ru>
2022-12-18 00:15:08 +0300

    Day 10 without sorting

---
+++ src/main/java/day10/Main.java
@@ -0,0 +1,49 @@
+package day10;
+
+import com.sun.source.tree.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Node binaryTree = new Node();
+        Node root = new Node(20);
+
+        binaryTree.setRoot(root);
+
+        binaryTree.addNode(14, root);
+        binaryTree.addNode(11, root);
+        binaryTree.addNode(5, root);
+        binaryTree.addNode(8, root);
+        binaryTree.addNode(16, root);
+        binaryTree.addNode(15, root);
+        binaryTree.addNode(18, root);
+        binaryTree.addNode(23, root);
+        binaryTree.addNode(22, root);
+        binaryTree.addNode(27, root);
+        binaryTree.addNode(24, root);
+        binaryTree.addNode(150, root);
+
+        binaryTree.sortNodes();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
--- src/main/java/day10/Node.java
+++ src/main/java/day10/Node.java
@@ -6,153 +6,91 @@ import java.util.List;
 
 public class Node {
 
-        private Node root;
-        int value; // Значение, которое находится внутри узла
-        Node left; // Левый потомок
-        Node right; // Правый потомок
+    Node root;
+    int value; // Значение, которое находится внутри узла
+    Node left; // Левый потомок
+    Node right; // Правый потомок
 
-        public Node(int value, Node left, Node right) {
-            this.value = value;
-            this.left = left;
-            this.right = right;
-        }
-
-        // Конструктор узла, не имеющего потомков
-        public Node(int value) {
-            this.value = value;
-            this.left = null;
-            this.right = null;
-        }
-
-    public int getRoot() {
-        return root.value;
+    // Пустой конструктор узла
+    public Node() {
     }
 
-    public void setRoot(Node node) {
+    // Конструктор узла, имеющего значения и потомков
+    public Node(int value, Node left, Node right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
 
-            if (this.root == null) {
-                this.root = node;
-                this.left = null;
-                this.right = null;
-            }
-            else {
-                System.out.println("Корневой узел уже создан");
-            }
-        }
+    // Конструктор узла, не имеющего потомков
+    public Node(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 
-        // Метод, добавляющий новый узел
-        public void addNode(Node node) {
+    // Метод добавления корневого узла
+    public void setRoot(Node root) {
+        this.root = root;
+    }
 
-            if (root == null) {
-                setRoot(node);
-            }
+    // Метод, добавляющий новый узел
+    public void addNode(int value, Node root) {
 
-            Node currentNode = root;
-
+        if (this.root.equals(root)) {
+            Node stepNode = this.root;
             while (true) {
-
-                Node step = stepNode(node, currentNode);
-
-                if (step == currentNode.right) {
-                    currentNode = currentNode.right;
-                } else if (step == currentNode.left){
-                    currentNode = currentNode.left;
-                } else if (step == node) {
-                    if ((isNewNodeMoreOrSameThanCurrentNode(node, currentNode) == true) &&
-                            (isCurrentNodeHasRightHeir(currentNode) == false)) {
-                        currentNode.right = node;
-                        break;
-                    } else if ((isNewNodeMoreOrSameThanCurrentNode(node, currentNode) == false) &&
-                            (isCurrentNodeHasLeftHeir(currentNode) == false)) {
-                        currentNode.left = node;
-                        break;
-                    }
+                // Случай 1: У исследуемого узла значение больше чем вводимое
+                //           и нет потомков слева
+                if (stepNode.value - value > 0 && stepNode.left == null) {
+                    stepNode.left = new Node(value);
+//                    System.out.println(stepNode.left.value);
+                    break;
+                }
+                // Случай 2: У исследуемого узла значение или равно чем вводимое
+                //           и нет потомков справа
+                else if (stepNode.value - value <= 0 && stepNode.right == null) {
+                    stepNode.right = new Node(value);
+                    System.out.println(stepNode.right.value);
+                    break;
+                }
+                // Случай 3: У исследуемого узла значение больше чем вводимое
+                //           и есть потомки слева
+                else if (stepNode.value - value > 0 && stepNode.left != null) {
+                    stepNode = stepNode.left;
+                    System.out.println(stepNode.value);
+//                    break;
+                }
+                // Случай 3: У исследуемого узла значение меньше или равно чем вводимое
+                //           и есть потомки справа
+                else if (stepNode.value - value <= 0 && stepNode.right != null) {
+                    stepNode = stepNode.right;
+                    System.out.println(stepNode.value);
+//                    break;
                 }
             }
-
+        } else {
+            System.out.println("Что-то не так");
         }
 
-        // Метод суммирующий значение узла и его потомков
-        public int nodeSum() {
+    }
 
-            int sum = value;
+    // Метод, сортирующий данные древа и возвращающий StringBuilder данных
+    public void sortNodes() {
+        StringBuilder dataList = new StringBuilder(root.value);
 
-            if(left != null) {
-                sum += left.nodeSum();
-            }
-
-            if(right != null) {
-                sum += right.nodeSum();
-            }
-
-            return sum;
+        if (this.root.left != null) {
+            dataList.append(this.root.left.value);
         }
+        System.out.println(dataList);
+    }
 
-        public String sortNodes() {
+    // Вспомогательный метод, добавляющий в массив дополнительный элемент
+    private static Integer[] append(Integer[] arr, int element) {
 
-            Integer[] arrayOfData = {root.value};
-            String stringOfData;
+        List<Integer> list = new ArrayList<>(Arrays.asList(arr));
+        list.add(element);
 
-            if (left != null) {
-                append(arrayOfData, left.value);
-            }
-            if (right != null) {
-                append(arrayOfData, right.value);
-            }
-            Arrays.sort(arrayOfData);
-            stringOfData = Arrays.toString(arrayOfData);
-
-            return stringOfData;
-        }
-
-        // Метод, сравнивающий новый узел и текущий
-        private boolean isNewNodeMoreOrSameThanCurrentNode(Node newNode, Node currentNode){
-
-            boolean answer = newNode.value >= currentNode.value;
-
-            return answer;
-        }
-
-        // Метод, проверяющий наличие правого наследника
-        private boolean isCurrentNodeHasRightHeir(Node currentNode) {
-
-            boolean answer = currentNode.right != null;
-
-            return answer;
-        }
-
-        // Метод, проверяющий левого наследника
-        private boolean isCurrentNodeHasLeftHeir(Node currentNode) {
-
-            boolean answer = currentNode.left != null;
-
-            return answer;
-        }
-
-        // Вспомогательный метод, возвращающий текущий узел во время проверки
-        private Node stepNode(Node newNode, Node currentNode) {
-
-            if (isNewNodeMoreOrSameThanCurrentNode(newNode, currentNode) == true){
-                if (isCurrentNodeHasRightHeir(currentNode) == true) {
-                    return currentNode.right;
-                } else {
-                    return newNode;
-                }
-            } else {
-                if (isCurrentNodeHasLeftHeir(currentNode) == true) {
-                    return currentNode.left;
-                } else {
-                    return newNode;
-                }
-            }
-        }
-
-        // Вспомогательный метод, добавляющий в массив дополнительный элемент
-        private static Integer[] append(Integer[] arr, int element){
-
-            List<Integer> list = new ArrayList<>(Arrays.asList(arr));
-            list.add(element);
-
-            return list.toArray(new Integer[0]);
-        }
+        return list.toArray(new Integer[0]);
+    }
 }
