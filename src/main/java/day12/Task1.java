91c38d0ffe992fd04c9ae7eb1830983b974bc8cf
Алексей <Vaas19.96@yandex.ru>
2022-12-22 23:46:12 +0300

    Day 12 is on development stage

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,32 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+
+        List<String> carList = new ArrayList<>();
+
+        carList.add("Kia");
+        carList.add("Chery");
+        carList.add("Toyota");
+        carList.add("Nissan");
+        carList.add("Mitsubishi");
+
+        for (Object obj: carList) {
+            System.out.println(obj);
+        }
+        System.out.println("-----------------------------------------------");
+
+        int MiddleOfTheList = carList.size() / 2;
+        carList.set(MiddleOfTheList, "Hundai");
+        carList.remove(0);
+        for (Object obj: carList) {
+            System.out.println(obj);
+        }
+
+
 
     }
 }
--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -1,7 +1,22 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task2 {
     public static void main(String[] args) {
+
+        List<Integer> oddNumbers = new ArrayList<>();
+
+        for (int i = 0; i <= 30; i++) {
+            oddNumbers.add(i);
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 300; i <= 350; i++) {
+            oddNumbers.add(i);
+            System.out.print(i + " ");
+        }
 
     }
 }
