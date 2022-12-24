62cb405001cec3ecd3c44c474afdac8ce259d06c
Алексей <Vaas19.96@yandex.ru>
2022-12-25 00:16:06 +0300

    Task 1 and 2 in Day 14 is Done

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,36 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        File file = new File("input.txt");
+
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] numbers = line.split(" ");
+
+            if(numbers.length != 10) {
+                throw new IllegalArgumentException();
+            }
+
+            int sum = 0;
+
+            for(String number: numbers) {
+                sum += Integer.parseInt(number);
+            }
+
+            System.out.println(sum);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Неккоректный входной файл");
+        }
     }
 }
