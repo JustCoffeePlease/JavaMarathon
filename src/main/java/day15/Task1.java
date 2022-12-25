9da850d38192bdcffcd5fc583f636099fb4e1211
Алексей <Vaas19.96@yandex.ru>
2022-12-25 17:06:20 +0300

    Day 15 is done

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,27 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("src/main/resources/shoes.csv");
+        Scanner scanner = new Scanner(file);
+
+        File fileOut = new File("src/main/resources/shoesResult.txt");
+        PrintWriter printWriter = new PrintWriter(fileOut);
+
+        while (scanner.hasNextLine()) {
+            String[] array = scanner.nextLine().split(";");
+
+            if (Integer.parseInt(array[2]) == 0) {
+                printWriter.println(array[0] + ", " + array[1] + ", " + array[2]);
+            }
+        }
+        printWriter.close();
+        scanner.close();
     }
 }
